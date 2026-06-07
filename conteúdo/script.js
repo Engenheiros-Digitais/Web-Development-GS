8be9747dcbd1f84f4d9e431922f18400d388f4fd
Enzo <devenzocm@gmail.com>
2026-06-07 18:26:57 -0300

    feat: adiciona mostrarResultado e reiniciarQuiz

--- conteúdo/script.js
+++ conteúdo/script.js
@@ -77,6 +77,13 @@ function proximaPergunta(){
 
 }
 
+function mostrarResultado(){
+    document.getElementById('pergunta-container').style.display = 'none';
+    document.getElementById('btn-proximo').style.display = 'none';
+    document.getElementById('resultado-quiz').style.display = 'block';
+    document.getElementById('texto-resultado').textContent = 'Você acertou' + pontuacao + 'de' + perguntas.lenght + 'perguntas!';
+
+}
 
 
 
